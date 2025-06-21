981ed2fc02293aa984fff35ce8b33c1fb4faf45d
Aykut <ayktshnbs@gmail.com>
2025-06-21 17:45:03 +0300

    SOME UPDATES WİTH GPT HELP )

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const display = document.getElementById('display');
+let shouldResetDisplay = false; 
 
 function resizeDisplayFont() {
     const maxFontSize = 80; 
     const minFontSize = 20;
-    const display = document.getElementById('display');
     let fontSize = maxFontSize;
 
     display.style.fontSize = fontSize + "px";
 
     while (display.scrollWidth > display.clientWidth && fontSize > minFontSize) {
-        fontSize -= 20;
+        fontSize -= 5;
         display.style.fontSize = fontSize + "px";
     }
 }
 
 function appendToDisplay(input) {
+    if (shouldResetDisplay) {
+        if (!isNaN(input) || input === '.') {
+            display.value = '';
+        }
+        shouldResetDisplay = false;
+    }
+
     display.value += input;
+    resizeDisplayFont();
 }
+
 function clearDisplay() {
     display.value = '';
+    shouldResetDisplay = false;
+    resizeDisplayFont();
 }
+
 function calculate() {
     try {
         display.value = eval(display.value);   
         resizeDisplayFont();
-
-        setTimeout(() => {
-            clearDisplay(); 
-        },10000); 
+        shouldResetDisplay = true; 
     } catch {
         display.value = "Error";
         resizeDisplayFont();
+        shouldResetDisplay = true;
     }
 }
 
